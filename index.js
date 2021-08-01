ce8554b4602bc9f823c0e7692a830898956e5bba
David Xu <62825020+dxaviud@users.noreply.github.com>
2021-08-01 17:24:46 -0400

    DbJson class

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-exports.dbjson = () => {
-    console.log("Hello from @dxaviud/dbjson");
-} 
+class DbJson {
+    constructor() {
+        console.log("Hello from @dxaviud/dbjson");
+        this.objectCache = new Map();
+    }
+
+    hasObject(identifier) {
+        if (this.objectCache.has(identifier)) {
+            return true;
+        }
+        //todo check if the object is in the filesystem
+        return false;
+    }
+
+    getObject(identifier) {
+        if (this.objectCache.has(identifier)) {
+            return this.objectCache.get(identifier);
+        }
+        return null;
+    }
+
+    setObject(identifier, object) {
+        this.objectCache.set(identifier, object);
+    }
+
+    persist() {
+        //todo persist the objects in the object cache to disk (file system)
+    }
+}
+
+export default new DbJson();
