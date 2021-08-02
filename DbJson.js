1bd1a1fe404e7c0736e7ad7a54b42b50faf3b50e
David Xu <62825020+dxaviud@users.noreply.github.com>
2021-08-02 17:21:26 -0400

    Progress

--- DbJson.js
+++ DbJson.js
@@ -5,41 +5,52 @@ import FileSystemManager from "./FileSystemManager.js";
 export default class DbJson {
     constructor(dataDir) {
         const cwd = process.cwd();
+        this.objectCache = new Map();
+        this.fsmanager = new FileSystemManager(cwd, dataDir);
         console.log(
             "Hello from @dxaviud/dbjson, your database has been initialized"
         );
         console.log("Data is stored under " + path.join(cwd, dataDir));
-        this.objectCache = new Map();
-        this.fsmanager = new FileSystemManager(cwd, dataDir + "/");
     }
 
-    has(identifier) {
+    async has(identifier) {
+        console.log("Checking if db has " + identifier);
         if (this.objectCache.has(identifier)) {
+            console.log(identifier + " found in cache");
             return true;
         }
-        if (this.fsmanager.has(identifier)) {
+        if (await this.fsmanager.has(identifier)) {
+            console.log(identifier + " found under " + this.dataDir);
             return true;
         }
+        console.log("Could not find " + identifier);
         return false;
     }
 
-    get(identifier) {
+    async get(identifier) {
+        console.log("Getting " + identifier);
         if (this.objectCache.has(identifier)) {
+            console.log(identifier + " retrieved from cache");
             return this.objectCache.get(identifier);
         }
-        if (this.fsmanager.has(identifier)) {
-            return this.fsmanager.get(identifier);
+        if (await this.fsmanager.has(identifier)) {
+            console.log(identifier + " retrieved from under " + this.dataDir);
+            return this.fsmanager.read(identifier);
         }
+        console.log("Could not find " + identifier + ", returning null");
         return null;
     }
 
     set(identifier, object) {
+        console.log("Setting " + identifier + " in cache");
         this.objectCache.set(identifier, object);
     }
 
-    persist() {
+    async persist() {
+        console.log("Persisting all objects in cache to the file system");
         for (const [identifier, object] of this.objectCache) {
-            this.fsmanager.write(identifier, object);
+            await this.fsmanager.write(identifier, object);
+            console.log("Persisted " + identifier);
         }
     }
 }
--- FileSystemManager.js
+++ FileSystemManager.js
@@ -1,37 +1,68 @@
 import fs from "fs/promises";
+import fsSync from "fs";
+import path from "path";
 
 export default class FileSystemManager {
-    constructor(rootPath) {
-        if (rootPath.charAt(rootPath.length - 1) !== "/") {
-            rootPath += "/";
-        }
+    constructor(rootPath, dataDir) {
         this.rootPath = rootPath; // the absolute path to the project's root directory
-    }
-
-    identifierToPath(identifier) {
-        return (
-            this.rootPath +
-            "dbdata/" +
-            identifier.split(".").join("/") +
-            ".json"
+        this.dataDir = dataDir; // the name of the directory that stores db data
+        const dataDirFullPath = path.join(rootPath, dataDir);
+        fsSync.mkdir(dataDirFullPath, { recursive: false }, (err) => {
+            if (err) {
+                console.log(dataDirFullPath + " directory already exists");
+            } else {
+                console.log("Created directory " + dataDirFullPath);
+            }
+        });
+        console.log(
+            "initialized file system manager with root path " +
+                rootPath +
+                " and data directory " +
+                dataDir
         );
     }
 
-    async has(identifier) {
-        const path = this.identifierToPath(identifier);
-        return await this.hasPath(path);
+    identifierToPath(identifier) {
+        const split = identifier.split(".");
+        split[split.length - 1] += ".json";
+        const resultPath = path.join(this.rootPath, this.dataDir, ...split);
+        console.log(
+            "Converted identifier " + identifier + " to path " + resultPath
+        );
+        return resultPath;
     }
 
-    async hasPath(path) {
+    async has(identifier) {
+        const filePath = this.identifierToPath(identifier);
+        return await this.hasPath(filePath);
+    }
+
+    async hasPath(filePath) {
         try {
-            await fs.open(path);
+            await fs.open(filePath);
+            console.log(filePath + " exists");
             return true;
         } catch {
+            console.log(filePath + " does not exist");
             return false;
         }
     }
 
-    async write(object, identifier) {}
+    async write(identifier, object) {
+        const filePath = this.identifierToPath(identifier);
+        await fs.writeFile(filePath, JSON.stringify(object));
+        console.log("Wrote " + identifier + " to " + filePath);
+    }
 
-    async read(identifier) {}
+    async read(identifier) {
+        const filePath = this.identifierToPath(identifier);
+        console.log("Reading from " + filePath);
+        if (this.hasPath(filePath)) {
+            const jsonString = await fs.readFile(filePath, "utf8");
+            console.log("Returning parsed JSON");
+            return JSON.parse(jsonString);
+        }
+        console.log(filePath + " does not exist, returning null");
+        return null;
+    }
 }
